6c216db92e2a2ddcca212d4dce52e9e5df5679e4
Bhargav Patil <142030874+jaytpatil@users.noreply.github.com>
2023-09-02 18:55:55 +0530

    Add files via upload

---
+++ sum_of_even_no.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,sum=0;
+    cout<<"enter value of n : ";
+    cin>>n;
+    
+        for(int i=1;i<=n;i++){
+           sum=sum+i*2;
+           cout<<"sum is "<<sum<<endl;
+
+        }
+       
+return 0;
+    
+}
